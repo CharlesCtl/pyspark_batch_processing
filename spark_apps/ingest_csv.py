e41921ab2ebada5848fbda0c216c60e29e39d945
Carlos Catalan <charlesing92@gmail.com>
2024-06-04 17:23:41 -0600

    Added Log4j properties and python logger class

--- spark_apps/ingest_csv.py
+++ spark_apps/ingest_csv.py
@@ -1,16 +1,24 @@
 from pyspark.sql import *
+from lib.logger import Log4j
+
 
 if __name__ == "__main__":
     spark = SparkSession.builder\
         .appName("Hello Spark")\
         .getOrCreate()
+    """
     print("Hello from pyspark")
     sample_data = [{"name": "John    D.", "age": 30},
         {"name": "Alice   G.", "age": 25},
         {"name": "Bob  T.", "age": 35},
         {"name": "Eve   A.", "age": 28}]
-
+    
     df = spark.createDataFrame(sample_data)
     df.show()
     df.printSchema()
+    """
+    logger = Log4j(spark)
+    logger.info("It works from Hello Spark!!!")
+
+    logger.info("Finished Hello Spark")
     spark.stop()
